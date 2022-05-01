16bd6d88e119ee9b1f221194f667945bfb0ab482
joaohdallago <joaohdallago@gmail.com>
2022-05-01 12:23:40 -0300

    feat: check status

--- src/app.js
+++ src/app.js
@@ -119,4 +119,22 @@ app.get('/messages', async (req, res) => {
   }
 });
 
+app.post('/status', async (req, res) => {
+  const { user } = req.headers;
+
+  try {
+    const participant = await db.collection('participants').findOne({ name: user });
+
+    if (!participant) {
+      return res.sendStatus(404);
+    }
+
+    await db.collection('participants').updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+
+    return res.sendStatus(200);
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
 app.listen(5000);
